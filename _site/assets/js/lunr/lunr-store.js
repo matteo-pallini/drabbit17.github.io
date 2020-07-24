b4e7e8feb4a86841d2203b76ff21c6bf427554c2
drabbit17 <matteo.pallini17@gmail.com>
2020-07-24 16:03:04 +0100

    first commit for blog

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Sending multiple POST requests from a spidered page using Scrapy",
+        "excerpt":"Have you ever wanted to scrape the prices of your favourite hiking products website and while doing so realized that the price for a product may change, through an AJAX request, with changes in colour or size? It happened to me while building smarthiker.co.uk, a price comparison website for hiking,...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/post-requests-with-scrapy/",
+        "teaser": null
+      }]
